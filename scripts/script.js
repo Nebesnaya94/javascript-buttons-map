9662ba73a8b68982caa574f717ac791aa83a7257
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-08-31 04:29:31 +0300

    feat: add button events

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
+const buttons = document.querySelectorAll(".button");
 
+document.body.addEventListener("click", (e) => {
+  const buttonTarget = e.target.closest(".button");
+
+  if (buttonTarget) {
+    buttonTarget.classList.toggle("active");
+  } else {
+    for (let button of buttons) {
+      button.classList.remove("active");
+    }
+  }
+});
